b676087d6e657e06c2285117710f4b4d5fe88f5c
Hiram Torres <hiram@jtl.mx>
2022-07-23 17:43:04 -0600

    update usecase

---
+++ usecase/usecase.go
@@ -0,0 +1,19 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/javiertlopez/awesome/model"
+)
+
+// Assets interface
+type Assets interface {
+	Create(ctx context.Context, source string, public bool) (model.Asset, error)
+	GetByID(ctx context.Context, id string) (model.Asset, error)
+}
+
+// Videos interface
+type Videos interface {
+	Create(ctx context.Context, anyVideo model.Video) (model.Video, error)
+	GetByID(ctx context.Context, id string) (model.Video, error)
+}
