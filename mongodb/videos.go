f772a32e71577cb0fe51eae8913d2a246bdda31b
Hiram Torres <hiram@jtl.mx>
2025-03-23 19:30:55 -0600

    rename repositories

---
+++ mongodb/videos.go
@@ -0,0 +1,103 @@
+package mongodb
+
+import (
+	"context"
+	"time"
+
+	guuid "github.com/google/uuid"
+	"github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+
+	"github.com/javiertlopez/awesome/errorcodes"
+	"github.com/javiertlopez/awesome/model"
+)
+
+// Collection keeps the collection name
+const Collection = "videos"
+
+// video model for mongodb
+type video struct {
+	ID          string    `bson:"_id"`
+	Title       string    `bson:"title"`
+	Description string    `bson:"description"`
+	Duration    float64   `bson:"duration,omitempty"`
+	AssetID     string    `bson:"asset_id,omitempty"`
+	MasterID    string    `bson:"master_id,omitempty"`
+	CreatedAt   time.Time `bson:"createdAt"`
+	UpdatedAt   time.Time `bson:"updatedAt"`
+}
+
+// Create video creates a new ID, stores the video and returns the new object
+func (db *DB) Create(ctx context.Context, anyVideo model.Video) (model.Video, error) {
+	collection := db.mongo.Collection(Collection)
+	time := time.Now()
+
+	uuid := guuid.New().String()
+
+	insert := &video{
+		ID:          uuid,
+		Title:       anyVideo.Title,
+		Description: anyVideo.Description,
+		CreatedAt:   time,
+		UpdatedAt:   time,
+	}
+
+	if anyVideo.Asset != nil {
+		insert.AssetID = anyVideo.Asset.ID
+	}
+
+	_, err := collection.InsertOne(ctx, insert)
+	if err != nil {
+		db.logger.WithFields(logrus.Fields{
+			"step": "collection.InsertOne",
+			"func": "func (v *videos) Insert",
+			"uuid": uuid,
+		}).Error(err.Error())
+
+		return model.Video{}, err
+	}
+
+	return insert.toModel(), nil
+}
+
+// GetByID retrieves a video with the ID
+func (db *DB) GetByID(ctx context.Context, id string) (model.Video, error) {
+	var response video
+
+	collection := db.mongo.Collection(Collection)
+
+	filter := bson.M{"_id": id}
+
+	err := collection.FindOne(ctx, filter).Decode(&response)
+
+	if err != nil {
+		db.logger.WithFields(logrus.Fields{
+			"step": "collection.FindOne",
+			"func": "func (v *videos) GetByID",
+			"id":   id,
+		}).Error(err.Error())
+
+		if err == mongo.ErrNoDocuments {
+			return model.Video{}, errorcodes.ErrVideoNotFound
+		}
+
+		return model.Video{}, err
+	}
+
+	return response.toModel(), nil
+}
+
+func (v video) toModel() model.Video {
+	return model.Video{
+		ID:          v.ID,
+		Title:       v.Title,
+		Description: v.Description,
+		Asset: &model.Asset{
+			ID: v.AssetID,
+		},
+		Duration:  v.Duration,
+		CreatedAt: v.CreatedAt.String(),
+		UpdatedAt: v.UpdatedAt.String(),
+	}
+}
---
+++ muxinc/videos.go
@@ -0,0 +1,202 @@
+package muxinc
+
+import (
+	"context"
+	"encoding/base64"
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	muxgo "github.com/muxinc/mux-go/v5"
+	"github.com/sirupsen/logrus"
+
+	"github.com/javiertlopez/awesome/model"
+)
+
+// asset struct
+type asset struct {
+	data muxgo.Asset
+}
+
+// Ingest send a source file url to mux.com
+// Returns a string Asset ID
+func (a *Assets) Create(ctx context.Context, source string, public bool) (model.Asset, error) {
+	var policy []muxgo.PlaybackPolicy
+
+	if public {
+		policy = append(policy, muxgo.PUBLIC)
+	} else {
+		policy = append(policy, muxgo.SIGNED)
+	}
+
+	asset, err := a.mux.AssetsApi.CreateAsset(muxgo.CreateAssetRequest{
+		Input: []muxgo.InputSettings{
+			{
+				Url: source,
+			},
+		},
+		PlaybackPolicy: policy,
+		Test:           a.test,
+	})
+
+	if err != nil {
+		a.logger.WithFields(logrus.Fields{
+			"step":   "AssetsApi.CreateAsset",
+			"func":   "Create",
+			"source": source,
+		}).Error(err.Error())
+
+		return model.Asset{}, err
+	}
+
+	return model.Asset{
+		ID: asset.Data.Id,
+	}, nil
+}
+
+// GetByID retrieves an asset from Mux.com by Asset ID
+func (a *Assets) GetByID(ctx context.Context, id string) (model.Asset, error) {
+	response, err := a.mux.AssetsApi.GetAsset(id)
+
+	if err != nil {
+		return model.Asset{}, err
+	}
+
+	body := asset{
+		data: response.Data,
+	}
+
+	asset := body.toModel()
+
+	if len(body.data.PlaybackIds) > 0 {
+		var source, poster, thumbnail string
+		playbackID := body.data.PlaybackIds[0].Id
+
+		switch body.data.PlaybackIds[0].Policy {
+		case muxgo.PUBLIC:
+			source = fmt.Sprintf(
+				"https://stream.mux.com/%s.m3u8",
+				playbackID,
+			)
+
+			poster = fmt.Sprintf(
+				"https://image.mux.com/%s/thumbnail.png?width=%s&height=%s&smart_crop=true&time=%s",
+				playbackID,
+				"1920",
+				"1080",
+				"7",
+			)
+
+			thumbnail = fmt.Sprintf(
+				"https://image.mux.com/%s/thumbnail.png?width=%s&height=%s&smart_crop=true&time=%s",
+				playbackID,
+				"640",
+				"360",
+				"7",
+			)
+		case muxgo.SIGNED:
+			token, err := a.signURL(playbackID, "v", asset.Duration, 0, 0)
+			if err != nil {
+				return model.Asset{}, err
+			}
+
+			source = fmt.Sprintf(
+				"https://stream.mux.com/%s.m3u8?token=%s",
+				playbackID,
+				token,
+			)
+
+			token, err = a.signURL(playbackID, "t", asset.Duration, 1920, 1080)
+			if err != nil {
+				return model.Asset{}, err
+			}
+
+			poster = fmt.Sprintf(
+				"https://image.mux.com/%s/thumbnail.png?token=%s",
+				playbackID,
+				token,
+			)
+
+			token, err = a.signURL(playbackID, "t", asset.Duration, 640, 360)
+			if err != nil {
+				return model.Asset{}, err
+			}
+
+			thumbnail = fmt.Sprintf(
+				"https://image.mux.com/%s/thumbnail.png?token=%s",
+				playbackID,
+				token,
+			)
+		}
+
+		asset.Poster = poster
+		asset.Thumbnail = thumbnail
+		asset.Sources = []model.Source{
+			{
+				Source: source,
+				Type:   "application/x-mpegURL",
+			},
+		}
+	}
+
+	return asset, nil
+}
+
+func (a *asset) toModel() model.Asset {
+	return model.Asset{
+		ID:                  a.data.Id,
+		CreatedAt:           a.data.CreatedAt,
+		Status:              a.data.Status,
+		Duration:            a.data.Duration,
+		MaxStoredResolution: a.data.MaxStoredResolution,
+		MaxStoredFrameRate:  a.data.MaxStoredFrameRate,
+		AspectRatio:         a.data.AspectRatio,
+		Passthrough:         a.data.Passthrough,
+	}
+}
+
+func (a *Assets) signURL(
+	playbackID string,
+	audience string,
+	duration float64,
+	width int,
+	height int,
+) (string, error) {
+	decodedKey, err := base64.StdEncoding.DecodeString(a.keySecret)
+	if err != nil {
+		return "", err
+	}
+
+	signKey, err := jwt.ParseRSAPrivateKeyFromPEM(decodedKey)
+	if err != nil {
+		return "", err
+	}
+
+	round := int(duration * 1.6)
+
+	claims := jwt.MapClaims{
+		"sub": playbackID,
+		"aud": audience,
+		"exp": time.Now().Add(time.Second * time.Duration(round)).Unix(),
+		"kid": a.keyID,
+	}
+
+	if audience == "t" {
+		claims["time"] = 7
+		claims["width"] = width
+		claims["height"] = height
+		claims["smart_crop"] = true
+	}
+
+	token := jwt.NewWithClaims(
+		jwt.SigningMethodRS256,
+		claims,
+	)
+
+	tokenString, err := token.SignedString(signKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
